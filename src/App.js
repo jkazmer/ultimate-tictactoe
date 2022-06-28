d3a91b0eb5757e7097ade9d64564521df21d169d
jkazmer <juliakazmer@gmail.com>
2022-06-28 11:38:19 -0400

    gameboard css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import MiniGrid from './MiniGrid';
+import './style.css';
+
 
 function App() {
   return (
-    null
+    <>
+      <div id='heading'>
+        <span id='ultimate'>ultimate </span>
+        <span id='tictactoe'>tic-tac-toe</span>
+      </div>
+      <div className='game-grid'>
+        <MiniGrid />
+        <MiniGrid />
+        <MiniGrid />
+        <MiniGrid />
+        <MiniGrid />
+        <MiniGrid />
+        <MiniGrid />
+        <MiniGrid />
+        <MiniGrid />
+      </div>
+    </>
   );
 }
 
---
+++ src/MiniGrid.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function MiniGrid() {
+    return (
+        <div className='mini-grid'>
+            <button></button>
+            <button></button>
+            <button></button>
+            <button></button>
+            <button></button>
+            <button></button>
+            <button></button>
+            <button></button>
+            <button></button>
+        </div>
+    )
+}
